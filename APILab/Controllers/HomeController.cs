2127d93a96488377b77d5bdfa6414a1e7cfbbce3
Yashashree Majalikar <yashmaja@gmail.com>
2021-11-03 00:41:45 -0400

    basic reqs met

---
+++ APILab/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using APILab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APILab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private CardsDAL cardsDAL = new CardsDAL();
+        private string deckID = "bp6k8j3xktqs";
+
+        public IActionResult DeckOfCards()
+        {
+            CardsModel result = cardsDAL.GetData(deckID);
+            if (result.remaining < 5)
+            { 
+                result = cardsDAL.ShuffleDeck(deckID);
+                result = cardsDAL.GetData(deckID);
+            }
+            return View(result);
+        }
+
+        /*public IActionResult SaveCards(bool keepCards, string code)
+        {
+            if 
+            CardsModel result = cardsDAL.AddToPile(deckID, code);
+            return RedirectToAction("DeckOfCards");
+        }*/
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ APILab/Models/CardsDAL.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace APILab.Models
+{
+    public class CardsDAL
+    {
+        public CardsModel GetData(string deckID)
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{deckID}/draw/?count=5";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            CardsModel result = JsonConvert.DeserializeObject<CardsModel>(JSON);
+
+            return result;
+        }
+
+        public CardsModel ShuffleDeck(string deckID)
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{deckID}/shuffle/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            CardsModel result = JsonConvert.DeserializeObject<CardsModel>(JSON);
+
+            return result;
+        }
+
+        public CardsModel AddToPile(string deckID, string value)
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{deckID}/pile/Keepers/add/?cards={value}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            CardsModel result = JsonConvert.DeserializeObject<CardsModel>(JSON);
+
+            return result;
+        }
+    }
+}
